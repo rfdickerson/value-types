359a3e5fc4a37306de42e951e65ce033bf663d37
Robert F. Dickerson <rfdickerson@us.ibm.com>
2016-04-18 15:09:58 -0500

    added benchmarking code

--- Sources/main.swift
+++ Sources/main.swift
@@ -5,13 +5,17 @@ enum Body {
 }
 
 struct Version {
+  
   var major: Int
   var minor: Int
+
 }
 
-class ResponseA {
+
+final class ResponseA {
 
   var body: Body
+  var headers = [String:String]()
 
 
   init(body: Body) {
@@ -23,15 +27,20 @@ class ResponseA {
 struct ResponseB {
 
   var body: Body
+  var headers = [String:String]()
 
 }
 
+/**
+ This method mutates Response in place
+ */
 func handle(_ response: ResponseA) {
 
   switch response.body {
     case .simpleText(let text):
     let newtext = text + "Additional information"
 
+    response.headers["OAuth"] = "token here"
     response.body = .simpleText(newtext)
   }
 }
@@ -41,8 +50,12 @@ func handle(_ response: ResponseB) -> ResponseB {
   switch response.body {
   case .simpleText(let text):
     let newtext = text + "Additional information"
+    var newheaders = response.headers
+    newheaders["OAuth"] = "token here"
 
-    let newResponse = ResponseB(body: .simpleText(newtext))
+    let newResponse = ResponseB(body: .simpleText(newtext),
+      headers: newheaders 
+      )
     return newResponse
   }
   
@@ -52,13 +65,44 @@ func handle(_ response: ResponseB) -> ResponseB {
 let payload = String(repeating: Character("~"), count: 5000000)
 
 var a = ResponseA(body: .simpleText(payload))
-var b = ResponseB(body: .simpleText(payload))
+var b = ResponseB(body: .simpleText(payload), headers: [String:String]())
 
-//for i in 0...100000 {
-//  b = handle( b)
-//}
+func getTime() 
+{
 
-for i in 0...100000 {
-  handle(a)
 }
-// print(b.body) 
+
+func timestamp() -> Double {
+
+  var tv = timeval()
+  gettimeofday(&tv, nil)
+  let t = Double(tv.tv_sec) + Double(tv.tv_usec)*1e-6
+  return t
+}
+
+func benchmark(_ function: (Void)->Void) -> Double {
+
+  let t1 = timestamp()
+  function()
+  let t2 = timestamp()
+
+  return t2-t1
+
+}
+
+let classTime = benchmark( {
+  for i in 0...1000000 {
+    handle(a)
+  }
+})
+
+let structTime = benchmark( {
+  for i in 0...1000000 {
+    handle(b)
+  }
+})
+
+
+print("Using structures was \(structTime) ms")
+
+print("Using classes was \(classTime) ms")
